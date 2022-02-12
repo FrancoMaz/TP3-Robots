4bc34343c799ccfb6d393e7e57cc7a6f0399ae07
Franco Mazzoni <franco.mazzoni@mercadolibre.com>
2022-02-12 17:44:36 -0300

    Get info from meli api

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import requests
+from result import Result
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/search')
+def search():
+    query = 'mate'
+    url = 'https://api.mercadolibre.com/sites/MLA/search?q=:' + query
+    x = requests.get(url).json()
+    results = x.get("results")
+    first_result = Result(results[0].get("thumbnail"), results[0].get("permalink"), query)
+    print(first_result.__str__())
+    return first_result.__str__()
 
 
 if __name__ == '__main__':
